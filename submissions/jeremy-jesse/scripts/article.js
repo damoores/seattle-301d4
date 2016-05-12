7a3ab7bbe76bb0829cb647cc940443d7942f083d
Jeremy Moore <damoores@gmail.com>
2016-05-12 14:22:45 -0700

    finsih TODOs. ADD EQUAL SIGN!!

--- submissions/jeremy-jesse/scripts/article.js
+++ submissions/jeremy-jesse/scripts/article.js
@@ -10,7 +10,9 @@ function Article (opts) {
 }
 
 Article.prototype.toHtml = function() {
-  // TODO: Use handlebars to render your articles!
+  var $source = $('#article-template').html();
+  var template = Handlebars.compile($source);
+  // TODO: DONE: Use handlebars to render your articles!
   //       - Select your template from the DOM.
   //       - Now "compile" your template with Handlebars.
 
@@ -22,8 +24,9 @@ Article.prototype.toHtml = function() {
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
 
-  // TODO: Use the function that Handlebars gave you to return your filled-in
+  // TODO: DONE: Use the function that Handlebars gave you to return your filled-in
   //       html template for THIS article.
+  return template(this);
 };
 
 ourLocalData.sort(function(a,b) {
